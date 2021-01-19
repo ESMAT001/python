138cfbaf7ff762dcdd34c60416ba5135147dc052
ESMAT001 <esmat10.niazi@gmail.com>
2021-01-19 09:22:24 +0430

    is_palindrome(is used to find the palindrome number) function added

--- palindrome_number.py
+++ palindrome_number.py
@@ -4,3 +4,13 @@ def len_num(n):
         len_count+=1
         n = n // 10
     return len_count
+
+def is_palindrome(number):
+    original_num = number
+    reverse_num = 0
+    while number > 0:
+        mod = number % 10
+        reverse_num = reverse_num * 10 + mod
+        number = number // 10
+        
+    return reverse_num == original_num
