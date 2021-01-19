e34b12c231c9850ccfef47a8023967e2362954bd
ESMAT001 <esmat10.niazi@gmail.com>
2021-01-19 09:24:08 +0430

    pl_of_none_pl(is used to find the palaindrom number of a none palindrome number) function added

--- palindrome_number.py
+++ palindrome_number.py
@@ -14,3 +14,31 @@ def is_palindrome(number):
         number = number // 10
         
     return reverse_num == original_num
+
+def pl_of_none_pl(number):
+    original_num = temp_num = number
+    n = len_original_num = len_num(number)
+    first_half_part = 0
+    
+    if n%2==0:
+        n=n/2
+    else:
+        n=(n//2)+1
+    l=0    
+    while number > 0:
+        
+        mod = number % 10
+        if l >= n:
+            first_half_part = first_half_part * 10 + mod
+        if len_original_num % 2 != 0:
+            if l < n-1:
+                temp_num = temp_num // 10
+        else:
+            if l < n:
+                temp_num = temp_num //10
+                
+        number = number // 10
+        l += 1 
+
+    temp_num = temp_num * ( 10 ** ( len_original_num // 2 ) )
+    return temp_num + first_half_part
