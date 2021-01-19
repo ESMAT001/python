e2196c13656e97973729984ed5cb1a11d797c216
ESMAT001 <esmat10.niazi@gmail.com>
2021-01-19 09:14:38 +0430

    python practice

--- all_longest_string.py
+++ all_longest_string.py
@@ -2,4 +2,5 @@ def all_long(arr):
     longest=[len(x) for x in arr]
     longest=[arr[x] for x,v in enumerate(longest) if v==max(longest)]
     return longest
-    
+
+all_long(["aba", "aa", "ad", "vcd", "aba"])
