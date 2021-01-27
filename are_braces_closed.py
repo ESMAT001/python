c9ea44e8f2b536ac9001b0e43d6c19921a88b9f9
ESMAT001 <esmat10.niazi@gmail.com>
2021-01-27 23:24:56 +0430

    are braces closed problem solved

---
+++ are_braces_closed.py
@@ -0,0 +1,21 @@
+def fn(x):
+    if x in ")]}":
+        return chr(ord(x)-2) if x in "]}" else chr(ord(x)-1)
+    else:
+        return x
+def are_braces_closed(txt):
+    braces="{[()]}"
+    txt = [x for x in txt if x in braces]
+    if len(txt)%2!=0:
+        return False
+    txt=list(map(fn,txt))
+    x=len(txt)
+    for i in range(len(txt)//2):
+        if txt[i]!=txt[x-1:x-2:-1][0]:
+            return False
+        x-=1
+    else:
+        return True
+
+res=are_braces_closed("{2+(2+3{2-[{}]})}")
+print(res)
